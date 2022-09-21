5be44273ffc036833373e1e37daf781fa64e666a
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-21 21:19:24 +0300

    apiCalls updated

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,9 +1,38 @@
 import axios from "axios";
 import { API_BASE_URL } from "../Constants";
 
-
-
-
 export const loginUser = (post) => {
-    return axios.post(API_BASE_URL + "/api/auth/login", post);
+  return axios.post(API_BASE_URL + "/api/auth/login", post);
+};
+
+export const createMessage = (payload, token) => {
+  return axios.post(API_BASE_URL + "/api/message", payload, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const updateMessage = (payload, token) => {
+  return axios.put(API_BASE_URL + "/api/message", payload, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const createAnnouncement = (payload, token) => {
+  return axios.post(API_BASE_URL + "/api/announcement", payload, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const updateAnnouncement = (payload, token) => {
+  return axios.put(API_BASE_URL + "/api/announcement", payload, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
