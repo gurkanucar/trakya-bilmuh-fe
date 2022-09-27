a5d1d8ea0222f491e6520d7c3855bd68d46df2bd
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-28 01:19:25 +0300

    AnnouncementList created

---
+++ src/pages/AnnouncementsPage/AnnouncementList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { AnnouncementListItem } from "./AnnouncementListItem";
+import "./AnnouncementsPage.css";
+
+export const AnnouncementList = (props) => {
+  const { announcementList, saveAnnouncement } = props;
+
+  return (
+    <div className="announcement-list">
+      {announcementList.size == 0 && <h2>Loading...</h2>}
+      {announcementList.map((x, idx) => (
+        <AnnouncementListItem
+          saveAnnouncement={saveAnnouncement}
+          key={idx}
+          message={x}
+        />
+      ))}
+    </div>
+  );
+};
