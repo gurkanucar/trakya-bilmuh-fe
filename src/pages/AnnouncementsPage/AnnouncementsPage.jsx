485a33542a8828a542d65c4baf6b14a01909d592
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-27 22:34:42 +0300

    announcement page created

---
+++ src/pages/AnnouncementsPage/AnnouncementsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AnnouncementsPage.css";
+export const AnnouncementsPage = (props) => {
+ 
+  const { credientals } = props;
+
+  return (
+    <div>
+      <h1>AnnouncementsPage</h1>
+    </div>
+  );
+};
