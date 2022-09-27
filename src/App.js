b9c04cdc26128f1cc7d467c0021ba7ca25bb9232
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-27 22:34:22 +0300

    header implemented to app js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,39 @@ import { LoginPage } from "./pages/LoginPage/LoginPage";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { PrivateRoute } from "./components/PrivateRoute";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { AnnouncementsPage } from "./pages/AnnouncementsPage/AnnouncementsPage";
+import { MessagesPage } from "./pages/MessagesPage/MessagesPage";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "./store/auth";
 
 function App() {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const credientals = useSelector((state) => state.auth.value);
+
+  const doLogout = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
+
+
   return (
-    <div>
-      <Routes>
+     <div>  
+       {credientals.isLoggedIn &&  <NavbarComponent doLogout={doLogout} credientals={credientals}/>} 
+        <Routes>
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/" element={<PrivateRoute />}>
-          <Route path="/home" element={<HomePage />} />
-
+          <Route path="/home" element={<HomePage credientals={credientals} />} />
+          <Route path="/messages" element={<MessagesPage />} />
+          <Route path="/announcements" element={<AnnouncementsPage />} />
+          <Route path="/settings" element={<HomePage />} />
           {/* <Route path="/addresses/details/:id" element={<DetailPage />} /> */}
         </Route>
         <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </div>
+      </Routes></div>
   );
 }
 
