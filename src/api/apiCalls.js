a95259f481d92c67323179d9db01150c2d24132a
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-27 23:19:25 +0300

    apiCalls updated

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { API_BASE_URL } from "../Constants";
+import {
+  API_BASE_URL
+} from "../Constants";
 
 export const loginUser = (post) => {
   return axios.post(API_BASE_URL + "/api/auth/login", post);
@@ -31,6 +33,43 @@ export const createAnnouncement = (payload, token) => {
 
 export const updateAnnouncement = (payload, token) => {
   return axios.put(API_BASE_URL + "/api/announcement", payload, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+
+
+export const getAnnouncementList = (token) => {
+  return axios.get(API_BASE_URL + "/api/announcement", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+
+export const getAnnouncementById = (id, token) => {
+  return axios.get(API_BASE_URL + "/api/announcement/" + id, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+
+export const getMessageList = (token) => {
+  return axios.get(API_BASE_URL + "/api/message", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+
+export const getMessageById = (id, token) => {
+  return axios.get(API_BASE_URL + "/api/message/" + id, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
