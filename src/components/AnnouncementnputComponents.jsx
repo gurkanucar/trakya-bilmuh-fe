3741a0d368a74c333f1fc3ff686979e9c33bda47
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-10-01 00:02:02 +0300

    required field removed

--- src/components/AnnouncementnputComponents.jsx
+++ src/components/AnnouncementnputComponents.jsx
@@ -54,12 +54,11 @@ export const AnnouncementnputComponents = (props) => {
         <Form.Group className="mb-3" size="lg" controlId="form.link">
           <Form.Label size="lg">Link</Form.Label>
           <Form.Control
-            required
             onChange={(e) => setValues({ ...values, link: e.target.value })}
             value={values.link}
             size="lg"
             type="text"
-            placeholder="yönlendirme linki"
+            placeholder="yönlendirme linki (keyfi)"
           />
         </Form.Group>
         <div className="d-grid gap-2">
